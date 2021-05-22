961feea3f3b388a406a4768db1ccd19cf4ecc72d
Terry O'Keefe <terry.okeefe87@gmail.com>
2021-05-21 19:55:27 -0500

    take inputs client.js and post to server.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,31 @@ console.log('js loaded!');
 $(document).ready(readyNow);
 
 function readyNow() {
+    // shows jQuery is loaded in console
     console.log('jQuery loaded');
+
+    // Click listeners
+    $('#submit').on('click', addInputs)
     
+}
+
+// function to take in inputs and post
+function addInputs() {
+    // gather input values into a new object
+    let newInputs ={
+        num1: $('#numOne').val(),
+        num2: $('#numTwo').val()
+    };
+    // console log to see that values get collected
+    console.log(newInputs); 
+
+    // make a post request with newInputs to send to server.js
+    $.ajax({
+        method: 'POST',
+        url: '/history',
+        data: newInputs // becomes req.body on server
+    }).then(response => {
+        // console log newInputs being sent to server.js
+        console.log(response);
+    });
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 // go get express
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // make a server called app
 const app = express();
@@ -8,10 +9,40 @@ const PORT = 5000;
 
 // serve static files when requested
 app.use(express.static('server/public'));
+// use bodyParser to find the data sent in post
+app.use(bodyParser.urlencoded({extended : true}));
+
+// empty array to take in new inputs
+let history = [];
+
+// post to add new data
+app.post('/history', (req, res) => {
+    // console log to check that we receive inputs
+    console.log('Received from inputs', req.body);
+    //push inputs into history array
+    history.push(req.body);
+    // send an OK status in terminal
+    res.sendStatus(200);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // start listening for connections
 app.listen(PORT, () => {
     // console log to show server is listening
     console.log('RUNNING ON PORT:', PORT);
-    
 })
